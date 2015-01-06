3715febd8e88f5de30e91cbcc2f489037318cc9d
Nightash <Nightash87@gmail.com>
2015-01-06 18:34:04 +0800

    item25

--- item25/item25.cpp
+++ item25/item25.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "../shared/widget.hpp"
 
 using std::cout;
@@ -7,8 +8,23 @@ using std::endl;
 /*
  * 
  */
-using MyWidget<>
+using MyWidget = Widget < std::string > ;
+
+MyWidget Foo( MyWidget mw )
+{
+	mw.Bark();
+	mw.SetT(std::string("Hello World"));
+	return mw;
+}
+
 int main()
 {
+	MyWidget mw;
+
+	cout << "\n\nBeginFoo\n";
+	auto s = Foo(mw);
+	cout << "\n\nEndFoo\n";
+
+	s.Report();
     return 0;
 }
--- shared/widget.hpp
+++ shared/widget.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <array>
+#include <string>
 
 template<typename T>
 class Widget
@@ -8,44 +10,54 @@ class Widget
 public:
 	Widget()
 	{
+		++arr_statics_[FT_CTOR];
 		std::cout<<__FUNCTION__ <<'\n';
 	}
 
 	explicit Widget( const T& t ):t_(t) 
 	{
+		++arr_statics_[FT_CTOR];
 		std::cout<<__FUNCTION__ <<" : "<< t_ << "\n";
 	}
 
 	~Widget()
 	{
+		++arr_statics_[FT_DCTOR];
 		std::cout<<__FUNCTION__ <<'\n';
 	}
 
 	Widget( const Widget& w )
 	{
+		++arr_statics_[FT_COPY_CTOR];
 		t_ = w.t_;
-		std::cout<<__FUNCTION__ <<'\n';
+		std::cout<<__FUNCTION__ <<"copy ctor"<<'\n';
 	}
 
 	Widget( Widget&& rW )
 	{
+		++arr_statics_[FT_MOVE_CTOR];
 		t_ = std::move(rW.t_);
-		std::cout<<__FUNCTION__ <<'\n';
+		std::cout<<__FUNCTION__ << "move ctor"<<'\n';
 	}
 
 	Widget& operator=(const Widget& rhs )
 	{
-		std::cout<<__FUNCTION__ <<'\n';
+		++arr_statics_[FT_COPY_ASS];
+		t_ = rhs.t_;
+		std::cout<<__FUNCTION__ <<"copy assign"<<'\n';
 	}
 
 	Widget& operator=( Widget&& rhs )
 	{
-		std::cout<<__FUNCTION__ <<'\n';
+		++arr_statics_[FT_MOVE_ASS];
+		t_ = std::move(rhs.t_);
+		std::cout<<__FUNCTION__ <<"move assign"<<'\n';
 	}
 
-	void SetT(const T& t)
+	template<typename arg>
+	void SetT(arg&& t)
 	{
-		t_ = t;
+		t_ = std::forward<arg>(t);
 	}
 
 	void Bark() const 
@@ -53,6 +65,43 @@ public:
 		std::cout<<__FUNCTION__ << " : " << t_ <<'\n';
 	}
 
+	void Report() const
+	{
+		cout << "\nReport:";
+		for (int i = 0; i < FT_NUM; i++)
+		{
+			cout << FunTypeDesc(static_cast<FUN_TYPE>(i)) << " :" << arr_statics_[i] << endl;
+		}
+	}
+private:
+	enum FUN_TYPE
+	{
+		FT_CTOR = 0,
+		FT_COPY_CTOR,
+		FT_COPY_ASS,
+		FT_MOVE_CTOR,
+		FT_MOVE_ASS,
+		FT_DCTOR,
+		FT_NUM
+	};
+
+	std::string FunTypeDesc( FUN_TYPE f ) const
+	{
+		static std::string arr[] = 
+		{
+			
+				"FT_CTOR = 0",
+				"FT_COPY_CTOR",
+				"FT_COPY_ASS",
+				"FT_MOVE_CTOR",
+				"FT_MOVE_ASS",
+				"FT_DCTOR",
+				"FT_NUM"
+			
+		};
+		return arr[static_cast<int>(f)];
+	}
 private:
 	T t_;
+	mutable std::array<int,FT_NUM> arr_statics_ = {};
 };
