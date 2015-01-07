e28fb8b41b6ad45ccd0e299b1d9bc4787a429fb8
Nightash <Nightash87@gmail.com>
2015-01-07 10:36:47 +0800

    item25 正确写法

--- item25/item25.cpp
+++ item25/item25.cpp
@@ -10,11 +10,19 @@ using std::endl;
  */
 using MyWidget = Widget < std::string > ;
 
+// odd!
 MyWidget Foo( MyWidget mw )
 {
 	mw.Bark();
 	mw.SetT(std::string("Hello World"));
 	return mw;
+}
+
+// correct
+void FooV2( MyWidget&  mw)
+{
+	mw.Bark();
+	mw.SetT(std::string("Hello World"));
 }
 
 int main()
