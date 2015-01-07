2f0ccab8952815b070e37845ac99cb08722a9910
Nightash <Nightash87@gmail.com>
2015-01-07 10:53:10 +0800

    item26

--- item26/item26.cpp
+++ item26/item26.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 
 using std::cout;
 using std::endl;
@@ -7,8 +7,34 @@ using std::endl;
 /*
  * 
  */
+class Person
+{
+public:
+
+	// template universal ref ctor
+	// greediest
+	// dont do this!
+	template<typename T>
+	explicit Person(T&& t)
+	: name_(std::forward<T>(t))
+	{
+		cout << "universal ctor:" << name_ << endl;
+	}
+
+	explicit Person( int idx ) 
+	: name_(std::to_string(idx)){}
+
+	Person(const Person& rhs) = default;
+	// Person(Person&& rhs) = default;
+
+private:
+	std::string name_;
+};
 
 int main()
 {
+	Person p("James");
+	// auto cloneOfP(p); //< toggle compile error 
+	auto cloneOfP(const_cast<const Person&>(p)); //< toggle ctor
     return 0;
 }
